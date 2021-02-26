4861aae17acf4c7726ea9a7dbed42940f7d824f4
shava2Curry <47783146+shava2C@users.noreply.github.com>
2021-02-26 19:40:14 +0900

    Propに分割代入を適用

--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 const ColorfulMessage = (props) => {
+  const { color, children } = props;
   console.log(props);
   const contentStyle = {
-    color: props.color,
+    color,
     fontSize: "18px"
   };
-  return <p style={contentStyle}>{props.children}</p>;
+  return <p style={contentStyle}>{children}</p>;
 };
 
 export default ColorfulMessage;
