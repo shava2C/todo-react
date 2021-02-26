5ccf0acc37a0a6edc2e4fd054665370e28ef499d
shava2Curry <47783146+shava2C@users.noreply.github.com>
2021-02-26 19:38:25 +0900

    Propsの基本的な使い方 Propsで条件を渡す 渡す側：コンポーネントに対して適当な値を書く 渡される側：引数として利用する ※タグで囲って、値を渡すこともできるchildrenを使えば

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import React from "react";
+import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
   const onClickButton = () => alert();
 
-  const contentStyle = {
-    color: "blue",
-    fontSize: "18px"
-  };
-
   return (
     <React.Fragment>
       <h1 style={{ color: "red" }}>ほげ</h1>
-      <p style={contentStyle}>hoge</p>
+      <ColorfulMessage color="blue">元気？</ColorfulMessage>
+      <ColorfulMessage color="pink">げんきっす</ColorfulMessage>
       <button onClick={onClickButton}>ボタン</button>
     </React.Fragment>
   );
