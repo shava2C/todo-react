d9eeb15544ec27d390c6a989e172c0be2f1c471d
shava2Curry <47783146+shava2C@users.noreply.github.com>
2021-02-26 19:20:12 +0900

    コンポーネントの詳細説明 リアクトにイベントを割り当てるときはキャメルケースでJSを書き込む CSSをつけることもできる(文字列で書き込み、{}は2つ書く)、コンスト作ってそこにまとめるのも可能

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
 const App = () => {
+  const onClickButton = () => alert();
+
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px"
+  };
+
   return (
     <React.Fragment>
-      <h1>ほげ</h1>
-      <p>hoge</p>
+      <h1 style={{ color: "red" }}>ほげ</h1>
+      <p style={contentStyle}>hoge</p>
+      <button onClick={onClickButton}>ボタン</button>
     </React.Fragment>
   );
 };
