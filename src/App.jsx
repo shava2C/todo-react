cfecaeb93d0609e166bb504dd1c584d2f54f94dc
shava2Curry <47783146+shava2C@users.noreply.github.com>
2021-03-03 20:00:32 +0900

    再レンダリング stateが変わったとき、prop自体がよみこまれてたとき、親のコンポーネントが変わったときに代わる

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlg, setFaceShowFlg] = useState(true);
+
+  console.log("最初" + num);
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(100);
+  const onClickFaceShow = () => {
+    setFaceShowFlg(!faceShowFlg);
+  };
   return (
     <React.Fragment>
       <h1 style={{ color: "red" }}>ほげ</h1>
@@ -13,6 +19,8 @@ const App = () => {
       <ColorfulMessage color="pink">げんきっす</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
       <p>{num}</p>
+      <button onClick={onClickFaceShow}>表示・非表示</button>
+      {faceShowFlg && <p>:-)</p>}
     </React.Fragment>
   );
 };
