7702f170f3b1c59bb76af767f7345f722531cd14
shava2Curry <47783146+shava2C@users.noreply.github.com>
2021-03-03 20:14:15 +0900

    関心の分離 usestateを使うと、stateがへんこうされたときの判定を行える

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
   const [num, setNum] = useState(0);
-  const [faceShowFlg, setFaceShowFlg] = useState(true);
+  const [faceShowFlg, setFaceShowFlg] = useState(false);
 
-  console.log("最初" + num);
   const onClickCountUp = () => {
     setNum(num + 1);
   };
   const onClickFaceShow = () => {
     setFaceShowFlg(!faceShowFlg);
   };
+
+  useEffect(() => {
+    if (num % 3 === 0) {
+      faceShowFlg || setFaceShowFlg(true);
+    } else {
+      faceShowFlg && setFaceShowFlg(false);
+    }
+  }, [num]);
+
   return (
     <React.Fragment>
       <h1 style={{ color: "red" }}>ほげ</h1>
