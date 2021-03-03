eb3bf057aeb50001fbbf0a91a607a37590d00cdc
shava2Curry <47783146+shava2C@users.noreply.github.com>
2021-03-03 19:48:17 +0900

    stateの追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
   const onClickButton = () => alert();
-
+  const [num, setNum] = useState(100);
   return (
     <React.Fragment>
       <h1 style={{ color: "red" }}>ほげ</h1>
       <ColorfulMessage color="blue">元気？</ColorfulMessage>
       <ColorfulMessage color="pink">げんきっす</ColorfulMessage>
       <button onClick={onClickButton}>ボタン</button>
+      <p>{num}</p>
     </React.Fragment>
   );
 };
