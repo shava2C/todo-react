3ad77a328186f8da416419fcf811aaf9141a1799
shava2Curry <47783146+shava2C@users.noreply.github.com>
2021-03-03 19:51:16 +0900

    stateを変更

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
   const [num, setNum] = useState(100);
   return (
     <React.Fragment>
       <h1 style={{ color: "red" }}>ほげ</h1>
       <ColorfulMessage color="blue">元気？</ColorfulMessage>
       <ColorfulMessage color="pink">げんきっす</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
       <p>{num}</p>
     </React.Fragment>
   );
